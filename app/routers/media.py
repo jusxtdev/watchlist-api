422bb7dc5c174bc07e971eeae2191418cd1b1f63
Dev Prajapati <pdev25214@gmail.com>
2026-04-14 14:51:25 +0000

    added update_media PUT endpoint

--- app/routers/media.py
+++ app/routers/media.py
@@ -22,3 +22,19 @@ def add_media(data : MediaCreate, db : Session = Depends(get_db)):
      db.commit()
      db.refresh(new_media)
      return new_media
+ 
+@router.put('/{media_id}')
+def update_media(media_id : int, data : MediaUpdate, db : Session = Depends(get_db)):
+    update_data = data.model_dump(exclude_unset=True)
+    existing = db.query(Media).where(Media.id == media_id).first()
+    if not existing:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Media not found"
+        )
+
+    for key, val in update_data.items():
+        setattr(existing, key, val)
+        
+    db.commit()
+    db.refresh(existing)
+    return existing
