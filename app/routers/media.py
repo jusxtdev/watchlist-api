cbb80b88eae64365a0075598f34603faf04698fd
Dev Prajapati <pdev25214@gmail.com>
2026-04-14 15:14:05 +0000

    Added DELETE route for media

--- app/routers/media.py
+++ app/routers/media.py
@@ -7,24 +7,23 @@ from app.schemas.media import MediaCreate, MediaUpdate
 
 router = APIRouter(prefix="/media", tags=["Media"])
 
-@router.get('/')
-def all_media(db : Session = Depends(get_db)):
+
+@router.get("/")
+def all_media(db: Session = Depends(get_db)):
     return db.query(Media).all()
 
-@router.post('/')
-def add_media(data : MediaCreate, db : Session = Depends(get_db)):
-     new_media = Media(
-         title = data.title,
-         watched = data.watched,
-         genre = data.genre
-     )
-     db.add(new_media)
-     db.commit()
-     db.refresh(new_media)
-     return new_media
- 
-@router.put('/{media_id}')
-def update_media(media_id : int, data : MediaUpdate, db : Session = Depends(get_db)):
+
+@router.post("/")
+def add_media(data: MediaCreate, db: Session = Depends(get_db)):
+    new_media = Media(title=data.title, watched=data.watched, genre=data.genre)
+    db.add(new_media)
+    db.commit()
+    db.refresh(new_media)
+    return new_media
+
+
+@router.put("/{media_id}")
+def update_media(media_id: int, data: MediaUpdate, db: Session = Depends(get_db)):
     update_data = data.model_dump(exclude_unset=True)
     existing = db.query(Media).where(Media.id == media_id).first()
     if not existing:
@@ -34,7 +33,19 @@ def update_media(media_id : int, data : MediaUpdate, db : Session = Depends(get_
 
     for key, val in update_data.items():
         setattr(existing, key, val)
-        
+
     db.commit()
     db.refresh(existing)
+    return existing
+
+
+@router.delete("/{media_id}")
+def delete_media(media_id: int, db: Session = Depends(get_db)):
+    existing = db.query(Media).where(Media.id == media_id).first()
+    if not existing:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Media Not Found"
+        )
+    db.delete(existing)
+    db.commit()
     return existing
